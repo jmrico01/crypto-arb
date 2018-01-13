6886480bd7fba0b5effe5870d3d3004584bba528
Jose M Rico <jrico@princeton.edu>
2018-01-12 23:13:26 -0500

    Optimize flag for GCC

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -2,7 +2,7 @@
 
 # Must be run from the main project directory.
 
-GCC_FLAGS="-std=c++11"
+GCC_FLAGS="-std=c++11 -O3"
 GCC_WARNING_FLAGS="-Wall"
 
 g++ $GCC_FLAGS $GCC_WARNING_FLAGS -o build/profit_paths src-cpp/profit_paths.cpp
